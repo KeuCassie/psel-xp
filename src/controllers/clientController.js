9ece9c48d4839499eaa59d7dbb218b97616f69bf
Kelly Cassiano <keukssie@hotmail.com>
2022-07-24 00:28:41 -0300

    criando rota de login

---
+++ src/controllers/clientController.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const clientRouter = express.Router();
+// const clientService = require('../services/clientServices');
+
+module.exports = {
+    clientRouter,
+};
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,7 +1,11 @@
 const express = require('express');
 require('express-async-errors');
 
+const { clientRouter } = require('../controllers/clientController');
+
 const routers = express.Router();
+
+routers.use('/login', clientRouter);
 
 
 module.exports = routers;
